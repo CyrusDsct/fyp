22c6d3b9066d917773ba106f1f92710839453925
tygtyg151 <154287897+tygtyg151@users.noreply.github.com>
2025-09-07 02:50:58 +0800

    Create binning.py

---
+++ binning.py
@@ -0,0 +1,14 @@
+#find file for importing csv values
+
+#input number of bins
+#input bin ranges
+
+#find bins using all binning methods
+
+#go through all bins to find difference
+
+#output most similar binning method
+#output bins for comparison
+
+#graph to show data distribution with manual + other bins?
+#bar? use arpit's thing? ???
